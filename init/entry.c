8c597c859c6303d81f9c1a334e1e012cb5dfec63
bhunix <bhunix@gmail.com>
2017-03-23 12:05:23 +0800

    change console_test

--- init/entry.c
+++ init/entry.c
@@ -5,7 +5,7 @@ void console_print_test ()
 {
     console_clear();
     console_print_str_with_color ("hello world\n", rc_black, rc_green);
-    console_print_hex (0x3456ff, rc_black, rc_green);
+    console_print_hex (0x1234abc9, rc_black, rc_green);
     console_print_char ('\n', rc_black, rc_green);
     console_print_dec (12345678, rc_black, rc_green);
 }
