d2e54ebe3eb7cdfc222857b84f42b5ca15ef391f
Jet.C <bhunix@gmail.com>
2017-04-21 19:00:40 +0800

    add

---
+++ include/debug.h
@@ -0,0 +1,4 @@
+#include "string.h"
+#include "console.h"
+
+void printk (const char *format, ...);
---
+++ include/vargs.h
@@ -0,0 +1,7 @@
+#ifndef __VARGS_H__
+#define __VARGS_H__
+typedef __builtin_va_list va_list
+#define va_start(p,arg) __builtin_va_start(p,arg)
+#define va_arg(p,t) __builtin_va_arg(p,t)
+#define va_end(p) __builtin_va_end(p)
+#endif
