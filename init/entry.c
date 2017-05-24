406187003f2eea771511f397d8e123a89b0b53e6
bhunix <bhunix@gmail.com>
2017-05-24 19:46:16 +0800

    test printk

--- init/entry.c
+++ init/entry.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "console.h"
 #include "string.h"
+#include "debug.h"
 
 void string_test ()
 {
@@ -8,22 +9,24 @@ void string_test ()
     char s2[10] = "world";
     console_clear();
     console_print_str_with_color (strcat(s, s2), rc_black, rc_green);
-    console_print_dec (strlen(s), rc_black, rc_green);
-    console_print_dec (strcmp(s, s), rc_black, rc_green);
+    console_print_dec_with_color (strlen(s), rc_black, rc_green);
+    console_print_dec_with_color (strcmp(s, s), rc_black, rc_green);
 }
 
 void console_print_test ()
 {
     console_clear();
     console_print_str_with_color ("hello world\n", rc_black, rc_green);
-    console_print_hex (0x1234abc9, rc_black, rc_green);
-    console_print_char ('\n', rc_black, rc_green);
-    console_print_dec (12345678, rc_black, rc_green);
+    console_print_hex (0x1234abc9);
+    console_print_char ('\n');
+    console_print_dec (12345678);
 }
 
 int kern_entry()
 {
     //console_print_test ();
-    string_test();
+    //string_test();
+    //console_clear();
+    printk("%-10d",1234);
     return 0;
 }
