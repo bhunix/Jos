ac991ed950b7ec8cb74ef66512394e83fc4b131e
bhunix <bhunix@gmail.com>
2017-05-24 19:45:42 +0800

    add x86.h

---
+++ include/x86.h
@@ -0,0 +1,18 @@
+#ifndef __LIB_X86_H__
+#define __LIB_X86_H__
+
+#define do_div(n, base) ({                                        \
+    unsigned long __upper, __low, __high, __mod, __base;        \
+    __base = (base);                                            \
+    asm("" : "=a" (__low), "=d" (__high) : "A" (n));            \
+    __upper = __high;                                            \
+    if (__high != 0) {                                            \
+        __upper = __high % __base;                                \
+        __high = __high / __base;                                \
+    }                                                            \
+    asm("divl %2" : "=a" (__low), "=d" (__mod)                    \
+        : "rm" (__base), "0" (__low), "1" (__upper));            \
+    asm("" : "=A" (n) : "a" (__low), "d" (__high));                \
+    __mod;                                                        \
+ })
+#endif
