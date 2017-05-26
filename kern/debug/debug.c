ac98a1dc263462f92f2ed2fb19a2ba7f8c2ea9af
bhunix <bhunix@gmail.com>
2017-05-26 17:28:14 +0800

    print %s

--- kern/debug/debug.c
+++ kern/debug/debug.c
@@ -3,7 +3,7 @@
 #include "x86.h"
 #define PRINTK_MAX_LEN 100
 
-void putch (int ch, void *ref)
+void putch (int ch, void *putdat)
 {
     console_print_char (ch);
 }
@@ -36,46 +36,39 @@ unsigned long long get_uint (va_list args, int lflag)
     return n;
 }
 
-void print_num (void(*putch)(int ch, void *ref), void *ref, unsigned long long num, int base, int width, int padc)
+void print_num (void(*putch)(int ch, void *putdat), void *putdat, unsigned long long num, int base, int width, int padc)
 {
     unsigned long long ret = num;
     unsigned mod = do_div (ret, base);
-#if 0
-    int mod = num%base;
-    ret = num/base;
-#endif
 
     if (num >= base){
-        print_num (putch, ref, ret, base, width-1, padc);
-    }else{
+        print_num (putch, putdat, ret, base, width-1, padc);
+    }else {
         while (--width > 0){
-            putch(padc, ref);
+            putch(padc, putdat);
         }
     }
 
-    putch ("0123456789abcdef"[mod], ref);
+    putch ("0123456789abcdef"[mod], putdat);
 }
 
-void vsprintfmt (void(*putch)(int ch, void *ref), const char *fmt, va_list args)
+void vsprintfmt (void(*putch)(int ch, void *putdat), void *putdat, const char *fmt, va_list args)
 {
+    int width, lflag, base;
     unsigned long long num;
-    void *ref=NULL;
-    char ch;
-    while (1)
-    {
-        while ((ch = *fmt++) != '%')
-        {
-            if (ch == '\0')
-            {
+    char ch, padc;
+    char *str=NULL;
+
+    while (1){
+        while ((ch = *fmt++) != '%'){
+            if (ch == '\0'){
                 return;
             }
-            putch (ch, ref);
+            putch (ch, putdat);
         }
 
-        int width, lflag, padc, base;
         padc = ' ';
-        width = 0;
-        lflag = 0;
+        width = lflag = 0;
         base = 10;
     
     reswitch:
@@ -88,12 +81,10 @@ void vsprintfmt (void(*putch)(int ch, void *ref), const char *fmt, va_list args)
                 padc = '0';
                 goto reswitch;
             case '1' ... '9':
-                for (;;++fmt)
-                {
+                for (;;++fmt){
                     width = width*10+(ch-'0');
                     ch = *fmt;
-                    if (ch < '0' || ch > '9')
-                    {
+                    if (ch < '0' || ch > '9'){
                         break;
                     }
                 }
@@ -105,15 +96,32 @@ void vsprintfmt (void(*putch)(int ch, void *ref), const char *fmt, va_list args)
                 lflag++;
                 goto reswitch;
             case 'c':
-                putch (va_arg (args, int), ref);
+                putch (va_arg (args, int), putdat);
                 break;
             case 's':
+                if ((str = va_arg(args, char*)) == NULL){
+                    str = "(null)";
+                }
+                if (width > 0 && padc != '-'){
+                    for (width -= strlen(str); width > 0; width--){
+                        putch (padc, putdat);
+                    }
+                }
+                for (;(ch = *str++) != '\0'; width--){
+                    if (ch < ' ' || ch > '~'){
+                        putch ('?', putdat);
+                    }else {
+                        putch (ch, putdat);
+                    }
+                }
+                for (; width > 0; width--){
+                    putch (' ', putdat);
+                }
                 break;
             case 'd':
                 num = get_int (args, lflag);
-                if ((long long)num < 0)
-                {
-                    putch('-', ref);
+                if ((long long)num < 0){
+                    putch('-', putdat);
                 }
                 base = 10;
                 goto number;
@@ -126,17 +134,21 @@ void vsprintfmt (void(*putch)(int ch, void *ref), const char *fmt, va_list args)
                 base = 8;
                 goto number;
             case 'p':
-                break;
+                putch ('0', putdat);
+                putch ('x', putdat);
+                num = (unsigned long long)(uintptr_t)va_arg(args, void *);
+                base = 16;
+                goto number;
             case 'x':
                 num = get_uint (args, lflag);
                 base = 16;
                 goto number;
         number:
-                print_num (putch, ref, num, base, width, padc);
+                print_num (putch, putdat, num, base, width, padc);
                 break;
 
             default:
-                putch ('%', ref);
+                putch ('%', putdat);
                 break;
         }
     }
@@ -147,7 +159,7 @@ void printk (const char *fmt, ...)
     va_list args;
 
     va_start (args, fmt);
-    vsprintfmt (putch, fmt, args);
+    vsprintfmt (putch, NULL, fmt, args);
     va_end (args);
 }
 
